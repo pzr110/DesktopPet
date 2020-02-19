0e72a3022929e5f2bdb700cdd7df772709ce15f8
pzr110 <1171228190@qq.com>
2020-02-19 11:20:29 +0800

    下拉刷新

--- app/src/main/java/com/pzr/desktoppet/MainActivity.java
+++ app/src/main/java/com/pzr/desktoppet/MainActivity.java
@@ -7,12 +7,18 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.SyncStateContract;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.blankj.utilcode.util.ToastUtils;
 //import com.lljjcoder.style.citylist.Toast.ToastUtils;
+import com.github.nuptboyzhb.lib.SuperSwipeRefreshLayout;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.pzr.desktoppet.utils.easyhttp.EasyHttp;
@@ -27,13 +33,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private SuperSwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
 
     private List<PetsBean> mPetsBeans;
     private PetsAdapter mAdapter;
 
     private Context mContext;
+    // Header View
+    private ProgressBar progressBar;
+    private TextView textView;
+    private ImageView imageView;
 
 
     @Override
@@ -55,14 +65,33 @@ public class MainActivity extends AppCompatActivity {
 /**
  * 下拉刷新
  */
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        mSwipeRefreshLayout.setOnPullRefreshListener(new SuperSwipeRefreshLayout.OnPullRefreshListener() {
             @Override
             public void onRefresh() {
-//                if (pageNum > 1) {
-//                    pageNum = 1;
-//                }
-                getInfo(true);
+                textView.setText("正在刷新");
+                imageView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+                new Handler().postDelayed(new Runnable() {
 
+                    @Override
+                    public void run() {
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, 2000);
+                getInfo(true);
+            }
+
+            @Override
+            public void onPullDistance(int i) {
+
+            }
+
+            @Override
+            public void onPullEnable(boolean enable) {
+                textView.setText(enable ? "松开刷新" : "下拉刷新");
+                imageView.setVisibility(View.VISIBLE);
+                imageView.setRotation(enable ? 180 : 0);
             }
         });
     }
@@ -77,7 +106,28 @@ public class MainActivity extends AppCompatActivity {
         View emptyView = getLayoutInflater().inflate(R.layout.item_empty, null);
         mAdapter.setEmptyView(emptyView);
         mRecyclerView.setAdapter(mAdapter);
-        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);//这个方法是设置SwipeRefreshLayout刷新圈颜色
+        mSwipeRefreshLayout.setHeaderViewBackgroundColor(0xff888888);
+        mSwipeRefreshLayout.setHeaderView(createHeaderView());// add headerView
+        mSwipeRefreshLayout.setTargetScrollWithLayout(true);
+//        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);//这个方法是设置SwipeRefreshLayout刷新圈颜色
+    }
+
+    /**
+     * create Header View
+     */
+    private View createHeaderView() {
+        //TODO 创建下拉刷新头部的View样式
+        View headerView = LayoutInflater.from(mSwipeRefreshLayout.getContext())
+                .inflate(R.layout.view_swipe, null);
+        progressBar = headerView.findViewById(R.id.pb_view);
+        textView = headerView.findViewById(R.id.text_view);
+        textView.setText("下拉刷新");
+        imageView = headerView.findViewById(R.id.image_view);
+        imageView.setVisibility(View.VISIBLE);
+        imageView.setImageResource(R.drawable.ic_down_arrow);
+        progressBar.setVisibility(View.GONE);
+
+        return headerView;
     }
 
     private void initViewId() {
@@ -98,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
                         ToastUtils.showShort("网路错误，请稍后重试");
                         if (mSwipeRefreshLayout != null && mSwipeRefreshLayout.isRefreshing()) {
                             mSwipeRefreshLayout.setRefreshing(false);
+
                         }
                     }
 
@@ -106,6 +157,7 @@ public class MainActivity extends AppCompatActivity {
 
                         if (mSwipeRefreshLayout != null && mSwipeRefreshLayout.isRefreshing()) {
                             mSwipeRefreshLayout.setRefreshing(false);
+
                         }
 
                         List<PetsBean> userList = GsonUtils.fromJson(s, new TypeToken<List<PetsBean>>() {
